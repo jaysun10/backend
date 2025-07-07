1b586225e2402c8ac02a88fb34aad05fdb177c80
jaysun <jaysun752@gmail.com>
2025-07-07 21:37:16 +0530

    Update profiles.js

--- data/profiles.js
+++ data/profiles.js
@@ -20,7 +20,7 @@ let profiles = [
     availability: "By Appointment",
     isPremium: true,
     contactInfo: {
-      whatsapp: "+1234567890",
+      whatsapp: "+1(731)259-3855",
       telegram: "@midnightqueenss",
       phone: "+1234567890"
     }
